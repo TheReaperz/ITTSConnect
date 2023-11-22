a354fd2581cc517849e5bd6b744784a00a8dee89
Bintang Satrio <tomiawan12@gmail.com>
2023-11-23 02:38:34 +0700

    minor fix

--- App.js
+++ App.js
@@ -22,7 +22,7 @@ export default function App() {
   return (
     <NativeBaseProvider>
       <NavigationContainer>
-      <Stack.Navigator initialRouteName="HomeCompanyScreen">
+      <Stack.Navigator initialRouteName="Splash">
           <Stack.Screen name="Splash" component={SplashScreen} options={{ headerShown: false }} />
           <Stack.Screen name="GetStarted" component={GetStartedScreen} options={{ headerShown: false }} />
           <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
