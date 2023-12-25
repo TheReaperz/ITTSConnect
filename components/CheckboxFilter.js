637c2d9390655859fcae6196dd77fd209a428cda
Bintang Satrio <tomiawan12@gmail.com>
2023-12-25 16:19:53 +0700

    Added Filter Feature on search Screen

--- components/CheckboxFilter.js
+++ components/CheckboxFilter.js
@@ -2,35 +2,46 @@ import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-const CheckboxFilter = () => {
-    const [selectedOptions, setSelectedOptions] = useState([]);
+const CheckboxFilter = ({ onSelectionChange, selectedOptions: initialSelectedOptions }) => {
+    const [selectedOptions, setSelectedOptions] = useState(initialSelectedOptions || []);
+
+    const options = [
+        { key: 'Fulltime', text: 'Fulltime' },
+        { key: 'Internship', text: 'Internship' },
+        { key: 'Assistant', text: 'Assistant' },
+        { key: 'Freelance', text: 'Freelance' },
+        { key: 'Part-Time', text: 'Part-Time' },
+    ];
 
     const handleOptionSelect = (option) => {
-        if (selectedOptions.includes(option)) {
-            setSelectedOptions(selectedOptions.filter((item) => item !== option));
+        const currentIndex = selectedOptions.indexOf(option);
+        const newSelectedOptions = [...selectedOptions];
+
+        if (currentIndex === -1) {
+            newSelectedOptions.push(option);
         } else {
-            setSelectedOptions([...selectedOptions, option]);
+            newSelectedOptions.splice(currentIndex, 1);
         }
+
+        setSelectedOptions(newSelectedOptions);
+        onSelectionChange(newSelectedOptions); // Update parent component
     };
 
     return (
         <View style={styles.container}>
             <Text style={styles.header}>Job Type</Text>
-            <TouchableOpacity onPress={() => handleOptionSelect('option1')}>
-                <Text>{selectedOptions.includes('option1') ? <Icon name="check-square-o" size={16} color="#000" /> : <Icon name="square-o" size={16} color="#000" />} Apprenticeship</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => handleOptionSelect('option2')}>
-                <Text>{selectedOptions.includes('option2') ? <Icon name="check-square-o" size={16} color="#000" /> : <Icon name="square-o" size={16} color="#000" />} Part-time</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => handleOptionSelect('option3')}>
-                <Text>{selectedOptions.includes('option3') ? <Icon name="check-square-o" size={16} color="#000" /> : <Icon name="square-o" size={16} color="#000" />} Full time</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => handleOptionSelect('option4')}>
-                <Text>{selectedOptions.includes('option4') ? <Icon name="check-square-o" size={16} color="#000" /> : <Icon name="square-o" size={16} color="#000" />} Contarct</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => handleOptionSelect('option5')}>
-                <Text>{selectedOptions.includes('option5') ? <Icon name="check-square-o" size={16} color="#000" /> : <Icon name="square-o" size={16} color="#000" />} Project-based</Text>
-            </TouchableOpacity>
+            {options.map((option) => (
+                <TouchableOpacity key={option.key} onPress={() => handleOptionSelect(option.key)}>
+                    <Text>
+                        {selectedOptions.includes(option.key) ? (
+                            <Icon name="check-square-o" size={16} color="#000" />
+                        ) : (
+                            <Icon name="square-o" size={16} color="#000" />
+                        )}
+                        {' ' + option.text}
+                    </Text>
+                </TouchableOpacity>
+            ))}
         </View>
     );
 };
--- screens/FilterScreen.js
+++ screens/FilterScreen.js
@@ -1,20 +1,51 @@
 import React, { useState } from 'react';
 import { View, TouchableOpacity, ScrollView, Image } from 'react-native';
 import { Container, Heading, Button, Text } from 'native-base';
-import Checkbox from '../components/CheckboxFilter';
-import RadioButton from '../components/RadioButtonFilter';
-import TabsNavigation from '../components/TabsNavigation';
+import CheckboxFilter from '../components/CheckboxFilter';
+import RadioButtonFilter from '../components/RadioButtonFilter';
 
-const FilterScreen = ({ navigation }) => {
+const FilterScreen = ({ onClose, onApplyFilters, currentFilters }) => {
+
+    const [selectedJobTypes, setSelectedJobTypes] = useState(currentFilters.jobTypes || []);
+    const [selectedExperience, setSelectedExperience] = useState(currentFilters.experience || null);
+
+    const handleJobTypeChange = (newSelectedJobTypes) => {
+        setSelectedJobTypes(newSelectedJobTypes);
+    };
+
+    const handleExperienceChange = (newExperience) => {
+        setSelectedExperience(newExperience);
+    };
+
+    const clearFilters = () => {
+        setSelectedJobTypes([]);
+        setSelectedExperience(null);
+
+        // Pass empty filter object back to SearchScreen
+        onApplyFilters({ jobTypes: [], experience: null });
+
+        // Close the modal
+        onClose();
+    };
 
     const handleSubmit = () => {
-        
+        // Construct filter object
+        const filters = {
+            jobTypes: selectedJobTypes,
+            experience: selectedExperience
+        };
+
+        // Pass filter object back to SearchScreen
+        onApplyFilters(filters);
+
+        // Close the modal
+        onClose();
     };
 
     return (
         <View style={{ flex: 1 }}>
             <Container paddingTop={45}>
-                <TouchableOpacity onPress={() => navigation.navigate('Search')}>
+                <TouchableOpacity onPress={onClose} >
                     <Image
                         source={require('../assets/Icons/back.png')}
                         style={{ width: 19, height: 15, marginLeft: 20 }}
@@ -25,9 +56,25 @@ const FilterScreen = ({ navigation }) => {
                 <Heading px={30}>Job Filter</Heading>
             </Container>
             <ScrollView style={{ flex: 1 }}>
-                <Checkbox />
-                <RadioButton />
+                <CheckboxFilter 
+                    onSelectionChange={setSelectedJobTypes} 
+                    selectedOptions={currentFilters.jobTypes}
+                />
+                <RadioButtonFilter 
+                    onSelectionChange={setSelectedExperience} 
+                    selectedOption={currentFilters.experience}
+                />
             </ScrollView>
+            <Button
+              mx={8}
+              rounded={15}
+              variant={"subtle"}
+              colorScheme="gray"
+              size="md"
+              onPress={clearFilters}
+            >
+              <Text bold color={"black"}>Clear Filter</Text>
+            </Button>
             <Button
               mt={4}
               mb={4}
@@ -39,7 +86,6 @@ const FilterScreen = ({ navigation }) => {
             >
               <Text bold color={"white"}>APPLY FILTER</Text>
             </Button>
-            <TabsNavigation navigation={navigation}/>
         </View>
     );
 };
